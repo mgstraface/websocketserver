307682e5d5c55b5a34fcd9dc9b93f10a73951cef
Matias Straface <matiasgstraface@gmail.com>
2023-05-23 12:15:14 -0300

    primera version

--- sockets/controller.js
+++ sockets/controller.js
@@ -2,7 +2,7 @@ const socketController = (socket) => {
 	console.log('Cliente conectado', socket.id);
 
 	socket.on('disconnect', () => {
-		console.log('Cliente desconectado', socket.id);
+		console.log('Cliente desconectado:', socket.id);
 	});
 
 	socket.on('enviar-mensaje', (payload, callback) => {
